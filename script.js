5a6e9709213845bfd8844898436bd3d6d47c335f
jc8080 <jc8080@rit.edu>
2020-06-18 15:26:44 -0400

    Addded a button at the top of the screen, moved the sketch to another div

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const container = document.getElementById("container");
+const grid = document.querySelector('.grid');
+const resetBtn = document.querySelector('.resetBtn');
 
 function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
+  grid.style.setProperty('--grid-rows', rows);
+  grid.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-
-
-
     cell.addEventListener('mouseover', e => e.target.style.backgroundColor = "black");
-
-
-    container.appendChild(cell).className = "grid-item";
+    grid.appendChild(cell).className = "grid-item";
   };
 };
 
+function addBtn(){
+  let button = document.createElement("button");
+  button.innerText = "reset";
+  resetBtn.appendChild(button).className = "button-item";
+}
+
 makeRows(16, 16);
+addBtn();
 
