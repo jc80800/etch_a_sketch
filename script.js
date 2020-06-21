bd100c0d6043aced0c75ba800538203d13c5ab54
jc8080 <jc8080@rit.edu>
2020-06-21 14:51:26 -0400

    Implemented reset button which removes all current grid, prompts user for number of squares and creates a new grid with the given input

--- script.js
+++ script.js
@@ -9,16 +9,24 @@ function makeRows(rows, cols) {
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
     cell.addEventListener('mouseover', e => e.target.style.backgroundColor = "black");
+    cell.classList.add('cell');
     grid.appendChild(cell).className = "grid-item";
   };
 };
 
-
-resetButton.addEventListener('click', reset());
-
 function reset(){
-  container.querySelectorAll('.cell').forEach(cell => cell.remove());
+  let grid = document.querySelector('.grid');
+  while (grid.firstChild){
+    grid.removeChild(grid.firstChild);
+  }
 }
 
-makeRows(32, 32);
+
+resetButton.addEventListener('click', () => {
+  reset();
+  let square = window.prompt("How many squares? (keep within range of 16 - 64");
+  makeRows(square, square);
+});
+
+makeRows(16, 16);
 
